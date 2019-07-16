49b400ee6b5fbe8c3c7c68e5830b2db8f8462022
Lippo <filippo.nardini@studio.unibo.it>
2019-07-16 16:26:08 +0000

    Create speaker default interface

--- 01-java-helloworld/src/main/java/demo/DeviceCapabilities.java
+++ 01-java-helloworld/src/main/java/demo/DeviceCapabilities.java
@@ -5,21 +5,13 @@ import org.protelis.vm.NetworkManager;
 import org.protelis.vm.impl.AbstractExecutionContext;
 import org.protelis.vm.impl.SimpleExecutionEnvironment;
 
-public class DeviceCapabilities extends AbstractExecutionContext {
+public class DeviceCapabilities extends AbstractExecutionContext implements Speaker {
 
     private final MyDeviceUID uid;
 
     public DeviceCapabilities(final int uid, final NetworkManager ntmgr) {
         super(new SimpleExecutionEnvironment(), ntmgr);
         this.uid = new MyDeviceUID(uid);
-    }
-
-    public void announce(String msg) {
-        System.out.println(msg);
-    }
-
-    public void announce(int a) {
-        System.out.println(a);
     }
 
     @Override
