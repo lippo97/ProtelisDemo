803e8dbae898df20fe0d7972ac7512f91f0a5bb3
Lippo <filippo.nardini@studio.unibo.it>
2019-07-18 10:56:31 +0000

    Rename kotlin socket to 04

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,4 @@ rootProject.name = "protelis-demo"
 include("01-java-helloworld")
 include("02-kotlin-helloworld")
 include("03-java-socket")
-include("03-kotlin-socket")
+include("04-kotlin-socket")
